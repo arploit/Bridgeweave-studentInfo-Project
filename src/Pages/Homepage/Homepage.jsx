2b9416a0e0e3b7f034c06967feac73cfcd58da72
arploit <arpesh_11@hotmail.com>
2024-01-31 11:18:01 +0530

    Removed Navigate Button

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import Home from '../../components/Home'
 
 const Homepage = () => {
-    const navigate = useNavigate()
-
-    const onNavigate = () => {
-        navigate('/info')
-    }
-
     return (
         <div>
             <Home />
-            <button onClick={onNavigate}>Navigate </button>
         </div>
     )
 }
